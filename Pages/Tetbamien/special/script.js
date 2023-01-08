f137cf520f23800766bf36336de5b29835cf948a
SPdream <37748496+SPdream99@users.noreply.github.com>
2023-01-09 02:44:14 +0700

    Add letter

---
+++ Pages/Tetbamien/special/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+    if (screen.availHeight > screen.availWidth) {
+        $(".envelope")[0].style.height = "18.5%";
+        $(".envelope")[0].style.transform = "scale(2)";
+        $(".paper")[0].style.height = "19.5%";
+        $(".paper")[0].style.transform = "scale(2)";
+    } else {
+        $(".envelope")[0].style.height = "77%";
+        $(".envelope")[0].style.transform = "scale(1)";
+        $(".paper")[0].style.height = "77%";
+        $(".paper")[0].style.transform = "scale(1)";
+    }
+    const wish = ["Chúc Tết đến trăm điều như ý", "Mừng xuân sang vạn sự thành công", "May mắn ngập tràn cả năm", "Cố gắng gấp đôi", "Gia đình hạnh phúc", "Niềm vui tràn trề", "Nỗ lực sẽ thành công"];
+    const random = Math.floor(Math.random() * wish.length);
+    document.getElementsByClassName("wish")[0].innerHTML = wish[random];
+    window.addEventListener("resize", rotateCaution);
+    element = document.querySelectorAll(".envelope");
+    element[0].addEventListener("click", () => {
+        element[0].classList.add("active");
+        createFirework();
+    });
+});
+
+function rotateCaution() {
+    if (screen.availHeight > screen.availWidth) {
+        $(".envelope")[0].style.height = "18.5%";
+        $(".envelope")[0].style.transform = "scale(2)";
+        $(".paper")[0].style.height = "19.5%";
+        $(".paper")[0].style.transform = "scale(2)";
+    } else {
+        $(".envelope")[0].style.height = "77%";
+        $(".envelope")[0].style.transform = "scale(1)";
+        $(".paper")[0].style.height = "77%";
+        $(".paper")[0].style.transform = "scale(1)";
+    }
+}
+
+var fw_spread = 250
+var fw_scale = 5
+var fw_launch_rate = 1000
+
+function createFirework(e) {
+    var f = document.createElement('div')
+    f.style.zIndex = '2';
+    f.className = 'firework'
+    f.style.width = '2px'
+    f.style.height = '2px'
+    f.style.position = 'absolute'
+    f.style.left = Math.random() * 33 + 33 + '%'
+    f.style.top = '100%'
+    var clr = 'hsl(' + Math.random() * 360 + 'deg,100%,50%)'
+    f.style.transition = Math.random() < .5 ? 'ease-out ' + 3 + 's' : 'ease-in ' + 2.5 + 's'
+
+    document.body.appendChild(f)
+
+    for (var i = 0; i < 25; i++) {
+        var p = document.createElement('div')
+        p.className = 'particle'
+        p.style.width = '100%'
+        p.style.height = '100%'
+        p.style.backgroundColor = clr
+        p.style.position = 'absolute'
+        p.style.left = '0'
+        p.style.top = '0'
+        p.style.transition = '.5s'
+        p.style.borderRadius = '50%'
+        f.appendChild(p)
+    }
+
+    setTimeout(function () {
+        f.style.top = Math.random() * 50 + 5 + '%'
+        f.ontransitionend = function () {
+            var p = this.querySelectorAll('.particle')
+            p.forEach(function (elm) {
+                var x = Math.random() < .5 ? Math.random() * fw_spread : (-1) * Math.random() * fw_spread
+                var y = Math.random() < .5 ? Math.random() * fw_spread : (-1) * Math.random() * fw_spread
+                elm.style.left = x + 'px'
+                elm.style.top = y + 'px'
+                elm.style.opacity = '0'
+                elm.style.transform = 'scale(' + fw_scale + ')'
+                document.body.style.setProperty('--bg-color', clr)
+                elm.ontransitionend = function () {
+                    this.remove()
+                }
+            })
+            setTimeout(function () {
+                f.remove()
+            }, 1000)
+        }
+    }, 100)
+
+    setTimeout(createFirework, fw_launch_rate)
+}
